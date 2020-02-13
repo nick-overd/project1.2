4c05d54b123ac315f92c24edc30124529548534f
nick-overd <nickoverd@gmail.com>
2020-02-13 22:08:29 +0000

    1.1

--- src/main/java/com/qa/database/runner/Runner2.java
+++ src/main/java/com/qa/database/runner/Runner2.java
@@ -15,25 +15,24 @@ import com.qa.databases.Order;
 public class Runner2 {
 
 	public static void main(String[] args) throws SQLException{
-		// customer
+
 		MysqlCustomerDao customerdao = new MysqlCustomerDao();
 		ArrayList<Customer> customers = customerdao.readAll();
 		for(Customer customer : customers) {
 			System.out.println(customer.getId() + " " + customer.getName());
 			}
-		//order
+		
 		MysqlOrderDao orderdao = new MysqlOrderDao();
 		ArrayList<Order> orders = orderdao.readAll();
 		for(Order order : orders) {
 			System.out.println(order.getOrderid() + " " + order.getCustomerid());
 		}
-		//item
+	
 		MysqlItemDao itemdao = new MysqlItemDao();
 		ArrayList<Item> items = itemdao.readAll();
 		for(Item item : items) {
 			System.out.println(item.getId() + " " + item.getName() + " " +  item.getCost());
 		}
-		//itemorder
 		MysqlItemOrderDao itemorderdao = new MysqlItemOrderDao();
 		ArrayList<ItemOrder> itemorders = itemorderdao.readAll();
 		for(ItemOrder ItemOrder : itemorders) {
